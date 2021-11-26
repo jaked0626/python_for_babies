837370c0c5ca44106bc16f539e996e53209af06c
Jake Underland <jakez0626@gmail.com>
2021-11-26 18:19:52 +0900

    first commit

---
+++ exercise1/exercise1.py
@@ -0,0 +1,84 @@
+
+
+def add_two_and_multiply(a, b):
+    """ Add 2 to a, and multiply by b"""
+
+    ### EXERCISE 1 -- YOUR CODE GOES HERE
+    # Replace "None" with the correct expression
+    r = None
+
+    ### DO NOT MODIFY THE FOLLOWING LINE!
+    return r
+
+
+def within_range(x, lb, ub):
+    """ Is x strictly between lb and ub?"""
+
+    ### EXERCISE 2 -- YOUR CODE GOES HERE
+    # Replace "None" with the correct expression
+    r = None
+
+    ### DO NOT MODIFY THE FOLLOWING LINE!
+    return r
+
+
+def number_string(x):
+    """
+    Given a number x, produce a string: "POSITIVE", "NEGATIVE", "ZERO"
+    (depending on whether the number is positive, negative, or zero)
+    """
+
+    ### EXERCISE 3 -- YOUR CODE GOES HERE
+    # Replace the following line with your code.
+    # After running your code, variable s should contain the value
+    # we ask you to compute in this exercise.
+    s = None
+
+    ### DO NOT MODIFY THE FOLLOWING LINE!
+    return s
+
+
+def num_divisible(lb, ub, p, q):
+    """
+    How many numbers between lb and ub (inclusive)
+    are divisible by both p and q?
+    """
+
+    ### EXERCISE 4 -- YOUR CODE GOES HERE
+    # Replace the following line with your code.
+    # After running your code, variable n should contain the value
+    # we ask you to compute in this exercise.
+    n = None
+
+    ### DO NOT MODIFY THE FOLLOWING LINE!
+    return n
+
+
+def count_even(lst):
+    """
+    Count the number of even numbers in the list
+    """
+
+    ### EXERCISE 5 -- YOUR CODE GOES HERE
+    # Replace the following line with your code.
+    # After running your code, variable n should contain the value
+    # we ask you to compute in this exercise.
+    n = None
+
+    ### DO NOT MODIFY THE FOLLOWING LINE!
+    return n
+
+
+def square_list(lst):
+    """
+    Produce a *new* list with its values squared
+    """
+
+    ### EXERCISE 6 -- YOUR CODE GOES HERE
+    # Replace the following line with your code.
+    # After running your code, variable n should contain the value
+    # we ask you to compute in this exercise
+    new_lst = None
+
+    ### DO NOT MODIFY THE FOLLOWING LINE!
+    return new_lst
---
+++ pa1/README.txt
@@ -0,0 +1,18 @@
+Programming Assignment #1
+-------------------------
+
+These are the files for Programming Assignment #1.
+This programming assignment was designed by the faculty of University of Chicago and does not belong to myself. 
+The instructions can be found at https://classes.cs.uchicago.edu/archive/2021/fall/12100-1/pa/pa1/index.html
+
+
+- sir.py: Skeleton code. You will write your code in this file.
+
+- test_sir.py: The automated tests. See the programming assignment
+  writeup for instructions on how to run them.
+
+- pytest.ini and .pylintrc: Configuration files that you can safely ignore.
+
+- tests/: A directory containing files specifying the tests.
+
+- README.txt: This file.
